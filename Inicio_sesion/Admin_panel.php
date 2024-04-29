aeb22b5401e95292de4e83cdc0e65bd5f1087d53
JoaquinAD1 <82043623+JoaquinMachXD2021@users.noreply.github.com>
2024-04-29 15:19:14 -0300

    Recopilación de datos
    Información, Imágenes y el logo de la empresa.

---
+++ Inicio_sesion/Admin_panel.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Inicio de sesión</title>
+        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
+        <link rel="stylesheet" type="text/css" href="mycss.css">
+        <script src="https://kit.fontawesome.com/cea831bcdf.js" crossorigin="anonymous"></script>
+
+    </head>
+    <boddy>
+        
+
+
+    </boddy>
+</html> 
