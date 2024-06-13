ee570b3d2b56b482a8f5cb5c21dc8b5586130776
JoaquinAD1 <82043623+JoaquinMachXD2021@users.noreply.github.com>
2024-06-13 19:01:45 -0300

    Diseño
    Diseño nuevo mas función contraseña

--- Inicio_sesion/Admin_panel.php
+++ Inicio_sesion/Admin_panel.php
@@ -1,31 +1,55 @@
 <html>
-    <head>
-        <title>Inicio de sesión</title>
-        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
-        <link rel="stylesheet" type="text/css" href="mycss.css">
-        <script src="https://kit.fontawesome.com/cea831bcdf.js" crossorigin="anonymous"></script>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>VaidrollTeamLogin6</title>
+     <link rel="stylesheet" href="vaidroll.css">	
+</head>
+<body>
+  <div class="cajafuera" align="center">
+ 
+<div class="formulariocaja">
+<form method="post" action="" name="vaidrollteam">
+<div class="formtitulo">Login</div>
+<div align="left" class="textoscajas">&#128273; Ingresar usuario</div>
+<input type="text" name="txtusuario" class="cajaentradatexto">
+<div align="left" class="textoscajas">
+&#128274; Ingresar password
+</div>
+<input type="password" name="txtpassword" id="txtpassword"
+ class="cajaentradatexto">
+ <div class="afcheckbox1" align="left">
+ <div style="float:left;">
+ <input type="checkbox" onclick="verpassword()" >
+ </div>
+ <div style="float:left;	color:white;">
+ Mostrar contraseña
+ </div>
+ </div>
+ 
+<input type="submit" value="Iniciar sesión" class="botonenviar">
 
-    </head>
-    <boddy>
-        <div class="login-from">
-            <h2>Panel de administración</h2>
-                <from> 
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Nombre de usuario">
+<div style="margin-top:5%;	color:white;">¿Necesitas una cuenta?  <a href="#">Registrar</a></div>
+</form>
+</div>
+<div class="autor">
+© 2021 Formulario Login. Todos los derechos reservados | Diseño de VaidrollTeam  
+<div>
+</div>
+</body>
 
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" placeholder="Contraseña">
+<script>
+  function verpassword(){
+      var tipo = document.getElementById("txtpassword");
+      if(tipo.type == "password")
+	  {
+          tipo.type = "text";
+      }
+	  else
+	  {
+          tipo.type = "password";
+      }
+  }
+</script>
 
-                    </div>
-
-                    <button type="submit"></button>
-
-                </from>
-        </div>
-
-    </boddy>
-</html> 
+</html>
