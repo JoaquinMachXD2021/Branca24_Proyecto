30bec985c3d536754865a1ebbcb0fbbaea1fc97c
JoaquinAD1 <82043623+JoaquinMachXD2021@users.noreply.github.com>
2024-08-07 21:32:08 -0300

    Actualización de datos en registro y login
    v23

---
+++ Login_and_Register/php/conxion_be.php
@@ -0,0 +1,14 @@
+<?php 
+    $conexion = mysqli_connect("127.0.0.1", "root", "", "login_register_db");
+    
+    /*
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+
+    }else{
+        echo 'No se ha podido conectar a la Base de Datos';
+    }
+    */
+
+
+?>
---
+++ Login_and_Register/php/registro_usuario_be.php
@@ -0,0 +1,70 @@
+<?php
+
+    include './conxion_be.php';
+    
+    $nombre_completo = $_POST['nombre_completo'];
+    $correo = $_POST['correo'];
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['contrasena'];
+
+    // Encriptar la contraseña.
+    $contrasena = hash('sha512', $contrasena);
+
+    // Verificar que el correo no se repita en la base de datos..
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo'");
+
+    if (mysqli_num_rows($verificar_correo) > 0) {
+        echo '
+            <script>
+                alert("Este correo ya está registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+
+    // Verificar que el usuario no se repita en la base de datos.
+    $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuario'");
+
+    if (mysqli_num_rows($verificar_usuario) > 0) {
+        echo '
+            <script>
+                alert("Este usuario ya está registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+
+    // Validar la longitud de la contraseña.
+    if (strlen($contrasena) < 8) {
+        echo '
+            <script>
+                alert("La contraseña debe tener al menos 8 caracteres.");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+
+    // Inserción en la base de datos.
+    $registroA = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena) 
+                VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena_encrip')";
+
+    $ejecutar = mysqli_query($conexion, $registroA);
+
+    if ($ejecutar) {
+        echo '<script>
+                alert("Usuario registrado exitosamente.");
+                window.location = "../index.php";
+            </script>';
+    } else {
+        // Imprimir error de MySQL para depuración.
+        echo '<script>
+                alert("Usuario no almacenado. Error: ' . mysqli_error($conexion) . '");
+                window.location = "../index.php";
+            </script>';
+    }
+
+    mysqli_close($conexion);
+?>
